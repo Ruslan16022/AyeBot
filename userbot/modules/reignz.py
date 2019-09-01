fe74754688d118e62688848c3375061a9e6bf8d6
Avinash Reddy <27774996+AvinashReddy3108@users.noreply.github.com>
2019-09-01 12:24:20 +0530

    Added Fake Scam Module
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

--- userbot/modules/reignz.py
+++ userbot/modules/reignz.py
@@ -164,6 +164,39 @@ async def json(event):
             await event.edit(f"`{the_real_message}`")
 
 
+@register(pattern=r".scam(?: |$)(.*)", outgoing=True)
+async def scam(event):
+    """ Just a small command to fake chat actions for fun !! """
+    if not event.text[0].isalpha() and event.text[0] not in (
+            "/", "#", "@", "!"):
+        options = ['typing', 'contact', 'game', 'location', 'voice', 'round', 'video', 'photo', 'document', 'cancel']
+        input_str = event.pattern_match.group(1)
+        args = input_str.split()
+        if len(args) is 0: # Let bot decide action and time
+            scam_action = random.choice(options)
+            scam_time = random.randint(30, 60)
+        elif len(args) is 1: # User decides time/action
+            try:
+                scam_action = str(args[0]).lower()
+                scam_time = random.randint(30, 60)
+            except ValueError:
+                scam_action = random.choice(options)
+                scam_time = int(args[0])
+        elif len(args) is 2: # User decides both action and time
+            scam_action = str(args[0]).lower()
+            scam_time = int(args[1])
+        else:
+            await event.edit("`Invalid Syntax !!`")
+            return
+        try:
+            if (scam_time > 0):
+                await event.delete()
+                async with event.client.action(event.chat_id, scam_action):
+                    await asyncio.sleep(scam_time)
+        except BaseException:
+            return
+
+
 CMD_HELP.update({
     'imdb': ".imdb <movie-name>\
     \nUsage: Shows movie info and other stuffs."
@@ -188,4 +221,9 @@ CMD_HELP.update({
 CMD_HELP.update({
     'json': '.json\
 \nUsage: Get detailed JSON formatted data about replied message.'
+})
+
+CMD_HELP.update({
+    'scam': '.scam <option>\
+\nUsage: Scam troll.'
 })
